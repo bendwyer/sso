be02f9ed7bdd38a5966e0f31e04f01d98e3e8098
Ben Dwyer <17102207+bendwyer@users.noreply.github.com>
2025-08-11 22:31:14 +0200

    Update terraform versions (#7)
    * Update version constraints
    
    * Add updated lockfile
    
    * Remove terraform required version constraint

--- terraform/modules/required-providers/versions.tf
+++ terraform/modules/required-providers/versions.tf
@@ -1,22 +1,24 @@
 terraform {
+  required_version = ">= 1.10.0"
+
   required_providers {
     auth0 = {
       source  = "auth0/auth0"
-      version = "1.20.1"
+      version = ">= 1.20.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "5.98.0"
+      version = ">= 5.98.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.52.0"
+      version = ">= 4.52.0, < 5.0.0"
       # lots of bugs, downgrading for now
-      # version = "5.5.0"
+      # version = ">= 5.5.0"
     }
     onepassword = {
       source  = "1Password/onepassword"
-      version = "2.1.2"
+      version = ">= 2.1.2"
     }
   }
 }
