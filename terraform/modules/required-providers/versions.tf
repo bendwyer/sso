a2bbba3c0e50d66f3a1c77d3a699f6f3c57f5e54
Ben Dwyer <17102207+bendwyer@users.noreply.github.com>
2025-09-27 12:17:22 +0200

    Move cloudflare version constraint to root. (#29)

--- terraform/modules/required-providers/versions.tf
+++ terraform/modules/required-providers/versions.tf
@@ -12,9 +12,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.52.0, < 5.0.0"
-      # lots of bugs, downgrading for now
-      # version = ">= 5.5.0"
+      version = ">= 4.52.0"
     }
     grafana = {
       source  = "grafana/grafana"
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -12,9 +12,8 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-      # lots of bugs, downgrading for now
-      # version = "~>5.0"
+      version = "~> 4.0, < 5.0.0"
+      # lots of bugs in 5.0, pinning to 4.0 for now
     }
     grafana = {
       source  = "grafana/grafana"
