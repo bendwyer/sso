8d306dfa29c616e3977cc84aee3d15770cef580e
bendwyer <17102207+bendwyer@users.noreply.github.com>
2026-05-27 21:52:56 +0000

    fix(hcp-terraform): incorrect placement of alias

--- terraform/modules/aws-iam-identity-center/versions.tf
+++ terraform/modules/aws-iam-identity-center/versions.tf
@@ -2,8 +2,7 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      configuration_aliases = [aws.MANAGEMENT]
+      source = "hashicorp/aws"
     }
     cloudflare = {
       source = "cloudflare/cloudflare"
